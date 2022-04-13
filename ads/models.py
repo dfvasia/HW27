12f5dea8606858191b4b8c41ff03f66188b295ce
dfvasia <vasilii-zhuravsk@mail.ru>
2022-04-13 06:41:23 +0300

    2

--- ads/models.py
+++ ads/models.py
@@ -2,7 +2,13 @@ from django.db import models
 
 # Create your models here.
 # Id,name,author,price,description,address,is_published
-
+class Advertisement(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False, unique=True)
+    author = models.CharField(max_length=200,null=False, blank=False)
+    price = models.PositiveSmallIntegerField(null=False, blank=False)
+    description = models.CharField(max_length=1000, null=False, blank=False)
+    address = models.CharField(max_length=1000, null=False, blank=False)
+    is_published = models.BooleanField(default=False)
 
 
 # id,name
