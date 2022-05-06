c38520c7eaf3e19f7b49068628828df26c3bf28e
Василий <vasilii-zhuravsk@mail.ru>
2022-05-06 09:39:19 +0300

    2

--- HW27/settings.py
+++ HW27/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt'
     'ads',
     'authentication'
 ]
@@ -141,5 +142,8 @@ TOTAL_ON_PAGE = 10
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework_simpleJWT.authentication.JWTAuthentication",
+    ],
 }
 AUTH_USER_MODEL = 'authentication.User'
